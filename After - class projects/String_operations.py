a53c1f4030b353621e574f9520ebb448412aac25
kavishchoudhury06-ux <kavish.choudhury06@gmail.com>
2026-04-14 18:56:08 +0530

    Add files via upload

---
+++ After - class projects/String_operations.py
@@ -0,0 +1,19 @@
+# String operations
+
+Greeting = "Good Morning, my name is Kavish Choudhury"
+
+print(len(Greeting))
+
+print(Greeting[4])
+
+print(Greeting[15:39])
+
+print(Greeting[1:21])
+
+print(Greeting [::-1])
+
+print(Greeting.lower())
+
+print(Greeting.upper())
+
+print(Greeting.capitalize())
